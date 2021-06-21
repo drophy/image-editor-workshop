d1bbb6461670854307362803b9963ff32ed5921d
drophy <drophyghost@gmail.com>
2021-06-21 00:39:14 -0500

    Day 3 Filters

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const height = 240;
 
 let uploadedImage = null;
 
+const downloadButton = $('#download-button');
+
+const redSlider = $('#red-slider');
+const greenSlider = $('#green-slider');
+const blueSlider = $('#blue-slider');
+
+const presetSelect = $('#preset-select');
+
 function setup() {
     createCanvas(width, height).parent('canvas-container');
     pixelDensity(1);
@@ -24,7 +32,7 @@ function setup() {
 }
   
 function draw() {
-    background(100);
+    background(100, 0);
     if(uploadedImage === null) return;
 
     let canvasRatio = width/height;
@@ -46,4 +54,61 @@ function draw() {
     }
     
     image(uploadedImage, x, y, w, h);
+
+    // Filters
+    loadPixels();
+
+    if(presetSelect.val() === 'grayscale') grayscale(pixels);
+    else defaultFilter(pixels);
+    
+    updatePixels();
+}
+
+downloadButton.click(function() {
+    uploadedImage.loadPixels();
+
+    // Backup pixel values
+    let pixelBackup = [];
+    for(let i = 0; i < uploadedImage.pixels.length; i++) {
+        pixelBackup.push(uploadedImage.pixels[i]);
+    }
+
+    // Apply filters
+    if(presetSelect.val() === 'grayscale') grayscale(uploadedImage.pixels);
+    else defaultFilter(uploadedImage.pixels);
+    
+    uploadedImage.updatePixels();
+
+    // Save
+    save(uploadedImage, 'edit.png');
+
+    // Restore image values
+    uploadedImage.loadPixels();
+    for(let i = 0; i < uploadedImage.pixels.length; i++) {
+        uploadedImage.pixels[i] = pixelBackup[i];
+    }
+    uploadedImage.updatePixels();
+});
+
+///// FILTERS /////
+function grayscale(pixels) {
+    for(let pixel = 0; pixel < pixels.length/4; pixel++) {
+        let i = pixel * 4;
+        let average = (pixels[i] + pixels[i+1] + pixels[i+2]) / 3;
+        pixels[i+0] = average; // R
+        pixels[i+1] = average; // G
+        pixels[i+2] = average; // B
+    }
+}
+
+function defaultFilter(pixels) {
+    let r = Number(redSlider.val());
+    let g = Number(greenSlider.val());
+    let b = Number(blueSlider.val());
+    for(let pixel = 0; pixel < pixels.length/4; pixel++) {
+      let i = pixel * 4;
+      pixels[i+0] = pixels[i+0] + r; // R
+      pixels[i+1] = pixels[i+1] + g; // G
+      pixels[i+2] = pixels[i+2] + b; // B
+    }
 }
